5d0eeef847f555630e464c9d088fe7765d36a258
Bagoes-Heikhal <67789235+Bagoes-Heikhal@users.noreply.github.com>
2021-11-11 11:48:34 +0700

    Menu 1 dan 2

--- Authentication_MCC59/Program.cs
+++ Authentication_MCC59/Program.cs
@@ -1,12 +1,97 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Authentication_MCC59
 {
     class Program
     {
+        public static List<UserData> types = new();
+        public static Dictionary<string, string> confidential = new();
+
         static void Main(string[] args)
+        {            
+            bool start = true;
+            while(start)
+            {
+                Menu();
+                int choose = NumInput();
+                switch (choose)
+                {
+                    case 1:
+                        InputData();
+                        break;
+                    case 2:
+                        ShowUserData();
+                        break;
+                    case 3:
+                        Login();
+                        break;
+                    case 4:
+                        start = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void InputData()
         {
-            Console.WriteLine("Hello Faiz");
+            Console.Write("Input first name : ");
+            string firstName = Console.ReadLine();
+            Console.Write("Input last name  : ");
+            string lastName = Console.ReadLine();
+            Console.Write("Input Paswword   : ");
+            string password = Console.ReadLine();
+
+            UserData user = new UserData(firstName, lastName, password);
+            confidential.Add(user.Id, user.Password);
+            types.Add(user);
+        }
+
+        public static void ShowUserData()
+        {
+            foreach (var item in types)
+            {
+                Console.WriteLine($"Full Name : {item.FirstName} {item.LastName}");
+                Console.WriteLine($"Password : {item.Password}");
+            }
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("1. Input");
+            Console.WriteLine("2. Show Data");
+            Console.WriteLine("3. Login");
+            Console.WriteLine("4. =========");
+        }
+        public static int NumInput()
+        {
+            int a = 0;
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please Enter a number !! " + e);
+            }
+            return a;
+        }
+        public static void Login()
+        {
+            Console.WriteLine("Input Id : ");
+            string id = Console.ReadLine();
+            Console.WriteLine("Input Password : ");
+            string pass = Console.ReadLine();
+
+            Console.WriteLine(confidential[id]);
+            Console.WriteLine(confidential[pass]);
+
+            //if (confidential[id] == id && confidential.Contains == pass)
+            //{
+
+            //}
+
         }
     }
 }
---
+++ Authentication_MCC59/UserData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication_MCC59
+{
+    class UserData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+        public string Password { get; set; }
+
+        public UserData(string firstName, string lastName, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            Id = firstName.Substring(0, 2) + lastName.Substring(0, 2);
+        }
+
+        //public static string StringConstrain()
+        //{
+        //    string name = "None";
+
+        //    string temp = Console.ReadLine();
+        //    if (temp.Length >= 2)
+        //    {
+        //        name = temp;   
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Pla");
+        //    }
+
+
+        //    return name;
+        //}
+            
+
+    }
+}
