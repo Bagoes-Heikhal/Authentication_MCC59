6def2e82b1c1f4b0d858866d04ff74018e29de31
Faiz <43031284+cilikan@users.noreply.github.com>
2021-11-11 14:08:10 +0700

    Charlie

---
+++ Authentication_MCC59/DataUsers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication_MCC59
+{
+    class DataUsers
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+        public DataUsers(string FName, string LName, string Pass)
+        {
+            FirstName = FName;
+            LastName = LName;
+            Password = Pass;
+            UserName = FName.Substring(0, 2) + LName.Substring(0, 2);
+        }
+    }
+}
--- Authentication_MCC59/Program.cs
+++ Authentication_MCC59/Program.cs
@@ -1,54 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Authentication_MCC59
 {
     class Program
     {
+        //Menyimpan Data dalam Bentuk List, Memanggil class DataUsers utk akses ke atribut
+        public static List<DataUsers> NewData = new();
+        //Menyimpan Data Username dan Password di Dictionary untuk keperluan searching dan login
+        public static Dictionary<string, string> AuthData = new();
         static void Main(string[] args)
         {
-            MainMenu();
-            int PilihMenu = InputConvert();
-            switch (PilihMenu)
+            bool StartProgram = true;
+            while (StartProgram)
             {
-                case 1:
-                    InsideCreate();
-                    break;
-                case 2:
-                    InsideShow();
-                    break;
-                case 3:
-                    InsideSearch();
-                    break;
-                case 4:
-                    InsideLogin();
-                    break;
-                case 5:
-                    InsideExit();
-                    break;
-                default:
-                    Console.WriteLine("Anda Memasukkan Angka Yang Salah");
-                    break;
-            }
-
-            
+                MainMenu();
+                int PilihMenu = InputConvert();
+                switch (PilihMenu)
+                {
+                    case 1:
+                        InsideCreate();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        InsideShow();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        InsideSearch();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        InsideLogin();
+                        Console.Clear();
+                        break;
+                    case 5:
+                        StartProgram = false;
+                        break;
+                    default:
+                        Console.WriteLine("Anda Memasukkan Angka Yang Salah");
+                        break;
+                }
+            }  
         }
         static int InputConvert()
         {
-            int Input = Convert.ToInt32(Console.ReadLine());
+            int Input = 0;
+            try
+            {
+                Input = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please Enter the Correct Format (Number 1-5) ! " + e);
+            }
             return Input;
         }
         static void MainMenu()
         {
             Console.WriteLine("++++++ Basic Authentication ++++++");
-            Console.WriteLine("1.\t Create User");
-            Console.WriteLine("2.\t Show User");
-            Console.WriteLine("3.\t Search User");
-            Console.WriteLine("4.\t Login User");
-            Console.WriteLine("5.\t Exit App");
+            Console.WriteLine("\t 1. Create User");
+            Console.WriteLine("\t 2. Show User");
+            Console.WriteLine("\t 3. Search User");
+            Console.WriteLine("\t 4. Login User");
+            Console.WriteLine("\t 5. Exit App");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.WriteLine(" ");
             Console.WriteLine("Pilih Menu Nomor : ");
         }
         static void InsideCreate()
         {
+            Console.Clear();
             Console.WriteLine("++++++ Create User ++++++");
             Console.Write("First Name : ");
             string InsertFirstName = Console.ReadLine();
@@ -56,24 +78,39 @@ namespace Authentication_MCC59
             string InsertLastName = Console.ReadLine();
             Console.Write("Password : ");
             string InsertPassword = Console.ReadLine();
-            Console.Clear();
+            DataUsers Users = new DataUsers(InsertFirstName, InsertLastName, InsertPassword);
+            NewData.Add(Users);
+            AuthData.Add(Users.UserName, Users.Password);
+            Console.WriteLine("+++++++++++++++++++++++++");
         }
         static void InsideShow()
         {
+            Console.Clear();
             Console.WriteLine("++++++ Show User ++++++");
-            Console.WriteLine("+++++++++++++++++++++++++++");
-            Console.WriteLine("Fullname : ");
-            Console.WriteLine("Username : ");
-            Console.WriteLine("Password : ");
-            Console.WriteLine("+++++++++++++++++++++++++++");
+            foreach (var n in NewData)
+            {
+                Console.WriteLine("+++++++++++++++++++++++++++");
+                Console.WriteLine("Fullname : " + n.FirstName + " " +n.LastName);
+                Console.WriteLine("Username : " + n.UserName);
+                Console.WriteLine("Password : " + n.Password);
+                Console.WriteLine("+++++++++++++++++++++++++++");
+            }
         }
         static void InsideSearch()
         {
+            Console.Clear();
             Console.WriteLine("++++++ Search User ++++++");
-            
+            Console.WriteLine("+++++++++++++++++++++++++++");
+            Console.Write("Username : ");
+            string SearchUsername = Console.ReadLine();
+            Console.WriteLine("Your Password is " + AuthData[SearchUsername]);
+            Console.WriteLine("+++++++++++++++++++++++++++");
+
+
         }
         static void InsideLogin()
         {
+            Console.Clear();
             Console.WriteLine("++++++ Login User ++++++");
             Console.WriteLine("+++++++++++++++++++++++++++");
             Console.Write("Username : ");
@@ -81,12 +118,19 @@ namespace Authentication_MCC59
             Console.WriteLine("Password : ");
             string LoginPassword = Console.ReadLine();
             Console.WriteLine("+++++++++++++++++++++++++++");
+            try
+            {
+                if(AuthData[LoginUsername] == LoginPassword)
+                {
+                    Console.WriteLine("\t Login Successful!");
+                }
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("\t Incorrect Username or Password, Try Again");
+            }
             
         }
-        static void InsideExit()
-        {
-            Console.WriteLine("Keluar");
-        }
-
+        
     }
 }
