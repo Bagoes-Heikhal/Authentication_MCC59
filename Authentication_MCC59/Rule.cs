5107948a90c8909d8b753431bee538c2c0d8a8a5
saniarhn <90261995+saniarhn@users.noreply.github.com>
2021-11-14 08:15:19 +0700

    edit dan konfirmasi pass

---
+++ Authentication_MCC59/Rule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication_MCC59
+{
+    class Rule
+    {
+        public static string StringConstrain(string name)
+        {
+            if (name.Length >= 2)
+            {
+                return name;
+            }
+            else
+            {
+                return name;
+            }
+        }
+    }
+}
